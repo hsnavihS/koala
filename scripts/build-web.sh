4d66436d208a34a194ce99dccfb43ec408817b3a
Shivansh <shuklashivansh26@gmail.com>
2025-04-22 14:48:55 +0530

    Point index to pick JS file from /site
    Attempting to deploy the playground site to pages, ideally I should set
    up a pipeline that does this but as a first attempt I'd like to see if I
    can get it working by just pushing the necessary files to the repo

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -13,6 +13,12 @@ fi
 emcmake cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -B ./build
 cmake --build ./build
 
+if [ ! -d "site" ]; then
+  echo "[BUILD] Creating site directory"
+  mkdir site
+fi
+mv ./build/koala.js ./build/koala.wasm ./site
+
 echo ""
 echo "[BUILD] Web build completed"
 
